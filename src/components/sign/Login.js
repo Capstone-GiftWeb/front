f5ed9e404f31da62c35d17a02d8c3aa432e8157d
JIHYUN <74723044+jihyun1971104@users.noreply.github.com>
2023-02-28 16:15:44 +0900

    login/signup페이지 틀 완성(start수정해야함)

--- src/components/sign/Login.js
+++ src/components/sign/Login.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import '../style/Login.css'
+import '../style/Signup.css'
 
 const Login = () => {
     const movePage = useNavigate();
+    const goToSignUp = () => movePage('/Signup');
 
-    const goToHome = () => movePage('/');
     return (
-        <>
-            <div className='login'>
-                This is a Login page!
+        <div className='container'>
+            <div className='form login'>
+                <h2>Log In</h2>
+                <div className='inputBox'>
+                    <input type="text" required="required"></input>
+                    <i class="fa-regular fa-user"></i>
+                    <span>username</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="password" required="required"></input>
+                    <i class="fa-solid fa-lock"></i>
+                    <span>password</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="submit" value="LOGIN"></input>
+                </div>
+                <p>Not Registered ?ㅤ
+                <button onClick={goToSignUp}>Create an account</button>
+                </p>
             </div>
-            <button onClick={goToHome}>Home</button>
-        </>
+        </div>
     )
 }
 
--- src/components/sign/Signup.js
+++ src/components/sign/Signup.js
@@ -2,17 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../style/Signup.css'
 
+
 const Signup = () => {
     const movePage = useNavigate();
+    const goToLogin = () => movePage('/Login');
 
-    const goToHome = () => movePage('/');
     return (
-        <>
-            <div className='signup'>
-                This is a Signup page!
+        <div className='container'>
+            <div className='form signup'>
+                <h2>Sign Up</h2>
+                <div className='inputBox'>
+                    <input type="text" required="required"></input>
+                    <i class="fa-regular fa-user"></i>
+                    <span>username</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="text" required="required"></input>
+                    <i class="fa-regular fa-envelope"></i>
+                    <span>email address</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="password" required="required"></input>
+                    <i class="fa-solid fa-lock"></i>
+                    <span>create password</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="password" required="required"></input>
+                    <i class="fa-solid fa-lock"></i>
+                    <span>confirm password</span>
+                </div>
+                <div className='inputBox'>
+                    <input type="submit" value="Create Account"></input>
+                </div>
+                <p>Already a member ?ㅤ
+                <button onClick={goToLogin}>Log in</button>
+                </p>
             </div>
-            <button onClick={goToHome}>Home</button>
-        </>
+        </div>
     )
 }
 
