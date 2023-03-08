367c8f3cbfe5a612d3041e0dfc60a394bb6714e9
Yunjeong Cho <dbswjd1413@naver.com>
2023-03-08 16:23:28 +0900

    Complete: Loading.js

---
+++ src/components/main/Products.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+//import '../style/lobby.css'
+
+const Products = () => {
+    const [loading, setLoading] = useState(false);
+
+    
+    return (
+        <>
+        </>
+    )
+}
+
+export default Products;
--- src/components/sign/Login.js
+++ src/components/sign/Login.js
@@ -18,44 +18,24 @@ const Login = () => {
       setInputPw(e.target.value);
     };
 
+    const data = {
+      email: inputId,
+      password: inputPw,
+      rememberId: true};
+
     const onClickLogin = () => {
         console.log("click login");
         console.log("ID : ", inputId);
-        console.log("PW : ", inputPw);
-        axios
-          .post("https://7db2-211-217-232-194.jp.ngrok.io/members/login", {
-            email: inputId,
-            passwd: inputPw,
-            rememberId: true
-          })
-        //   .then((res) => {
-        //     console.log(res);
-        //     console.log("res.data.userId :: ", res.data.userId);
-        //     console.log("res.data.msg :: ", res.data.msg);
-        //     if (res.data.email === undefined) {
-        //       // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
-        //       console.log("======================", res.data.msg);
-        //       alert("입력하신 id 가 일치하지 않습니다.");
-        //     } else if (res.data.email === null) {
-        //       // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
-        //       console.log(
-        //         "======================",
-        //         "입력하신 비밀번호 가 일치하지 않습니다."
-        //       );
-        //       alert("입력하신 비밀번호 가 일치하지 않습니다.");
-        //     } else if (res.data.email === inputId) {
-        //       // id, pw 모두 일치 userId = userId1, msg = undefined
-        //       console.log("======================", "로그인 성공");
-        //       sessionStorage.setItem("user_id", inputId); // sessionStorage에 id를 user_id라는 key 값으로 저장
-        //       sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
-        //     }
-        //     // 작업 완료 되면 페이지 이동(새로고침)
-        //     document.location.href = "/";
-        //   })
-        .then((res) => {
+        console.log("PW : ", inputPw)
+
+        axios.post("http://192.168.1.185:80/members/login", { data
+          },{ValidityState:false})
+        .then(() => {
             console.log("hi");
         })
-          .catch();
+          .catch(()=>{
+            console.log("bad");
+          });
       }
 
     return (
@@ -64,12 +44,12 @@ const Login = () => {
                 <h2>Log In</h2>
                 <div className='inputBox'>
                     <input type="text" name="input_id" value={inputId} onChange={handleInputId} required="required"></input>
-                    <i class="fa-regular fa-user"></i>
+                    <i className="fa-regular fa-user"></i>
                     <span>username</span>
                 </div>
                 <div className='inputBox'>
                     <input type="password" name="input_pw" value={inputPw} onChange={handleInputPw} required="required"></input>
-                    <i class="fa-solid fa-lock"></i>
+                    <i className="fa-solid fa-lock"></i>
                     <span>password</span>
                 </div>
                 <div className='inputBox'>
