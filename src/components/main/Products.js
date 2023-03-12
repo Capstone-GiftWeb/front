b3940b40017b4603ab3bd5d1acfbe79eb17c32c0
Yunjeong Cho <dbswjd1413@naver.com>
2023-03-13 00:38:05 +0900

    Update: Products1

--- src/components/main/Products.js
+++ src/components/main/Products.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import axios from 'axios';
 //import '../style/lobby.css'
 
 const Products = () => {
     const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
 
-    
+    let url = "https://localhost:8080/gifts/all";
+
+    useEffect(() => {
+        
+    },[]);
+
+    if(loading) return <Loading loading={loading}/>
+
     return (
         <>
+            this is a main page!
         </>
     )
 }
 
-export default Products;
+export default Products;d
