c154c6dc6cf3b11cf4a2a18df45a2d3167227ab3
Yunjeong Cho <dbswjd1413@naver.com>
2023-04-27 00:50:21 +0900

    Update: Category almost finish

--- src/components/main/Products.js
+++ src/components/main/Products.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import '../style/Products.css'
 
-const Products = ({ props , onClickProduct}) => {
+const Products = ({ props, onClickProduct }) => {
 
   return (
-    <div className='container'>
+    <div className='product-container'>
       <div className='row'>
         {
           props.map((product, index) => {
-            if (index < 10) {
-              return (
-                <div key={index} className="col-md-3 gird">
-                  <img src={`${product.image}`} alt="" width="100%" />
-                  <p onClick={() => onClickProduct(product.href)}>{product.title}</p>
-                </div>
-              );
-            }
+            return (
+              <div key={index} className="col-md-3 gird">
+                <img src={`${product.image}`} alt="" width="100%" />
+                <p onClick={() => onClickProduct(product.href)}>{product.title}</p>
+              </div>
+            );
+
           })
         }
       </div>
