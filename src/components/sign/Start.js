be57f9486b63270e1945bf4780362bc71f28dcbe
Yunjeong Cho <dbswjd1413@naver.com>
2023-05-17 02:37:01 +0900

    Update : font & recentProduct.css

--- src/components/sign/Start.js
+++ src/components/sign/Start.js
@@ -16,8 +16,8 @@ const Start = () => {
 
                 <section className='wishy'>
                     <div className='wishy-text'>
-                        <h5>#2 Trending</h5>
-                        <h4>Lead Supercell</h4>
+                        <h5>#Cap StoneAge</h5>
+                        <h4>FOR YOU</h4>
                         <h1>WishyMatch</h1>
                         <p>이 웹사이트는 캡-스톤 에이지 팀이 만들었음!</p>
                         <button onClick={goToLogin}>Login</button>
