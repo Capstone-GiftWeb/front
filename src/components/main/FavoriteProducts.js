14c7e09485642c88de4ce9fbf6afa90d2ec388db
jihyun <qkrwlgus7040@gmail.com>
2023-05-21 16:30:18 +0900

    Update: Proifle

---
+++ src/components/main/FavoriteProducts.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { HeartOutlined, HeartFilled } from '@ant-design/icons';
+import axiosInstance from '../..';
+import '../style/FavoriteProducts.css'
+
+const FavoriteProducts = ({ props, onClickProduct }) => {
+
+  const onClickFavorite = (product) => {
+    const href = product.href;
+    // 버튼 색 변경
+    product.favorite = !product.favorite
+
+    // 서버로 전송
+    axiosInstance({
+      method: "POST",
+      url: "/" + href,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        href
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className='favorite-body'>
+      <div className='container'>
+        <div className='row'>
+          {
+            props.map((product, index) => {
+              return (
+                <div key={index} className="product col-md-4 grid" onClick={() => { onClickProduct(product.href) }}>
+                  <img src={`${product.image}`} alt="" />
+                  <p className='product-title'>{product.title}</p>
+                    <div className='info'>
+                        <p className='product-price'>{product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</p>
+                        <div className='icons'>
+                        {product.favorite ?
+                        <HeartFilled style={{ color: "red", fontSize: "20px" }} onClick={() => onClickFavorite(product)} /> :
+                        <HeartOutlined style={{ fontSize: "20px" }} onClick={() => onClickFavorite(product)} />}
+                        </div>
+                    </div>
+                </div>
+              );
+
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+export default FavoriteProducts;
--- src/components/main/ProfileDetails.js
+++ src/components/main/ProfileDetails.js
@@ -3,11 +3,29 @@ import '../style/ProfileDetails.css';
 import { getCookie, removeCookie, setCookie } from "../utils/Cookie";
 import axiosInstance from "../..";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from 'react';
+import { getProducts } from "../utils/Data";
+import { setRecentHistory, filterDataByList, deleteRecentHistory, redirectPage } from '../utils/ClickUtils'
+import Loading from "./Loading";
+import FavoriteProducts from "./FavoriteProducts";
 
 const ProfileDetails = () => {
 
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const setScrollRef = useRef(0);
+
   const movePage = useNavigate();
+
+  const onClickProduct = (href) => {
+    setRecentHistory(href); // 로컬 스토리지에 저장
+
+    const filtered = filterDataByList(data); // 리스트로 데이터 필터링
+    setFilteredData(filtered); // 필터링된 데이터 설정
+
+    redirectPage(href); // 클릭한 값을 서버로 전송 > 리다이렉트!
+}
 
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -24,6 +42,28 @@ const ProfileDetails = () => {
 
     fetchProfileData();
   }, []);
+
+// Data.js의 getProducts를 사용하여 데이터를 불러와 useState에 저장
+useEffect(() => {
+    const fetchData = async () => {
+        setLoading(true);
+        const res = await getProducts();
+        if (res) { // res가 undefined인 경우에는 setData를 실행하지 않음
+            setData(res.gifts);
+        }
+        setLoading(false);
+    };
+    fetchData();
+}, []);
+
+useEffect(() => {
+    if (data.length > 0) {
+        const filtered = filterDataByList(data);
+        setFilteredData(filtered);
+    }
+}, [data])
+
+if (loading) return <Loading loading={loading} />
 
   const name = getCookie("name");
   const email = getCookie("email");
@@ -51,9 +91,7 @@ const ProfileDetails = () => {
 
     return(
         <div className="content-pf">
-            <div className="title">
-                <h2>Profile</h2>
-            </div>
+            <div className="color"></div>
             <div className="information">
               <div className="profile">
                   <div className="user">
@@ -69,11 +107,18 @@ const ProfileDetails = () => {
                       </div>
                   </div>
               </div>
-            <div className="like">
-              <p>Like♥</p>
+              <div className="like">
+                <div className='horizontal-box'>
+                  <div className='scroll-box' ref={setScrollRef}>
+                    <p>Like♥</p>
+                    <div className='like-products'>
+                      <FavoriteProducts props={data} onClickProduct={onClickProduct} />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
-      </div>
     )
 }
 
