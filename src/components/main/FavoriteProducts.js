e9d3bf49ea2b5423f8318d9488fd5cdf0b32d29c
Yunjeong Cho <dbswjd1413@naver.com>
2023-05-29 14:52:54 +0900

    Update : FavoiriteProducts.js & css

--- src/components/main/FavoriteProducts.js
+++ src/components/main/FavoriteProducts.js
@@ -3,6 +3,7 @@ import { HeartOutlined, HeartFilled } from '@ant-design/icons';
 import axiosInstance from '../..';
 import Modal from './Modal';
 import ModalPortal from './Portal';
+import { setClickFavorite } from '../utils/ClickUtils'
 import '../style/FavoriteProducts.css'
 
 const FavoriteProducts = ({ props, onClickProduct }) => {
@@ -21,12 +22,9 @@ const FavoriteProducts = ({ props, onClickProduct }) => {
             props.map((product, index) => {
               return (
                 <div key={index} className="likeproduct col-md-6 grid" onClick={() => { setClickModalItem(product); onClickProduct(product.href); handleModal(); }}>
-                  <ModalPortal>
-                     {modalOn && <Modal onClose={handleModal} item={clickModalItem} />}
-                  </ModalPortal>
                   <img src={`${product.image}`} alt="" />
                   <div className='info'>
-                    <img src='img/btnX.png' alt="deleteRecentItem" className='btnX' onClick={() => {}}/>
+                    <img src='img/btnX.png' alt="deleteRecentItem" className='btnX' onClick={() => { }} />
                     <p className='product-title'>{product.title}</p>
                     <p className='product-price'>{product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</p>
                   </div>
@@ -36,6 +34,9 @@ const FavoriteProducts = ({ props, onClickProduct }) => {
           }
         </div>
       </div>
+      <ModalPortal>
+        {modalOn && <Modal onClose={handleModal} item={clickModalItem} onClickFavorite={setClickFavorite} />}
+      </ModalPortal>
     </div>
   );
 
